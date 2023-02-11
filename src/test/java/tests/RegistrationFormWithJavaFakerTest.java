00be82f0da761c6cc6379c08a29bd5b62239d42d
vladislavkadyrov <vlad-kad00@mail.ru>
2023-02-11 17:39:49 +0500

    Add JavaFaker

---
+++ src/test/java/tests/RegistrationFormWithJavaFakerTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class StudentRegistrationFormWithPageObjectsTest extends TestBase {
+
+    @Test
+    void successfulRegistration() {
+        registrationPage.openPage()
+                .setFirstName("Vladislav")
+                .setLastName("Kadyrov")
+                .setUserEmail("vlad-kad00@mail.ru")
+                .setGenger("Male")
+                .setUserNumber("9992484959")
+                .setBirthDate("10", "July", "2000")
+                .setSubjects("Computer Science")
+                .setHobbies("Sports")
+                .uploadPicture("img/kwoe.png")
+                .setCurrentAddress("SD9H 1JQ")
+                .selectState("Uttar Pradesh")
+                .selectCity("Lucknow")
+                .clickSubmitButton()
+                .registrationResultsModal()
+                .verifyResult("Student Name", "Vladislav Kadyrov")
+                .verifyResult("Student Email", "vlad-kad00@mail.ru")
+                .verifyResult("Gender", "Male")
+                .verifyResult("Mobile", "9992484959")
+                .verifyResult("Date of Birth", "10 July,2000")
+                .verifyResult("Subjects", "Computer Science")
+                .verifyResult("Hobbies", "Sports")
+                .verifyResult("Picture", "kwoe.png")
+                .verifyResult("Address", "SD9H 1JQ")
+                .verifyResult("State and City", "Uttar Pradesh Lucknow");
+    }
+}
